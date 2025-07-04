6245dc9e781b677fedbd7fc96b536176ed279acc
GailChang <gail863@gmail.com>
2025-07-05 01:41:57 +0800

    feat: 新增計時功能，允許用戶輸入秒數並倒數顯示

--- time_practice.py
+++ time_practice.py
@@ -1,15 +1,17 @@
+"""Module providing a function to wait for specific time."""
 import time
 
 my_time = int(input('請輸入秒數: '))
 
+# 正著數
 # for x in range(my_time):
 #     print(x+1)
 #     time.sleep(1)
 
 # 倒著數
-# for x in range(my_time, 0, -1):
-#     print(x)
-#     time.sleep(1)
+for x in range(my_time, 0, -1):
+    print(x)
+    time.sleep(1)
 
 for x in range(my_time, 0, -1):
     # 想要的格式: 02:00
@@ -19,4 +21,3 @@ for x in range(my_time, 0, -1):
     time.sleep(1)
 
 print("時間到了")
-
