ffafd52f341e1ce2da41d14ccec4d79da77ac362
GailChang <gail863@gmail.com>
2025-07-05 12:49:31 +0800

    feat: Python 集合型別練習

--- list_sets_tuple.py
+++ list_sets_tuple.py
@@ -1,21 +1,29 @@
+"""Module providing the basic concept of list, set, and tuple."""
+
+# ==================
+#        list
+# ==================
+
+# 可以重複，且有順序
+# list 是一個有序的集合，可以包含重複的元素
 fruits = ['apple', 'orange', 'banana', 'coconut']
-print(fruits[0])
-print(fruits[0:-1])
+# print(fruits[0])
+# print(fruits[0:-1])
 
 fruits.append('guava')
 fruits.remove('coconut')
 
-for f in fruits:
-    print(f)
+# for f in fruits:
+    # print(f)
 
-print(fruits.index('banana'))
+# print(fruits.index('banana'))
 
 fruits.append('apple')
 fruits.append('apple')
-print(fruits.count('apple'))
+# print(fruits.count('apple'))
 
 fruits.reverse()
-print(fruits)
+# print(fruits)
 
 # ==================
 #        set
@@ -23,15 +31,19 @@ print(fruits)
 
 # 不能重複，且無順序
 
-good_set = {'apple', 'orange', 'banana', 'banana', 'coconut'}
+good_set = {'🍎', '🍊', '🍌', 'banana', 'coconut'}
+good_set.add('coconut')  # 重複的元素不會被添加
 
 # 即使輸入重複的，也會被忽略掉
 for s in good_set:
     print(s, end=" ")
 
 good_set.add('watermelon')
-if 'apple' in good_set:
+if 'watermelon' in good_set:
     print('it exists')
+
+good_set.remove('banana')  # 移除元素
+print(good_set)
 
 # ==================
 #     tuple 元組
@@ -42,4 +54,4 @@ print(many_tuple.count('apple'))
 print(many_tuple.index('orange'))
 
 # 不能在宣告後增刪
-many_tuple.add('apple')
+# many_tuple.add('apple')
