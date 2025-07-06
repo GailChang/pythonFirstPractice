3f6ec98e92d06cd4687de7ffc2f2e499bb04ffab
GailChang <gail863@gmail.com>
2025-07-06 17:20:13 +0800

    feat: 新增模組介紹及計算面積的功能，包括平方、立方及圓形面積計算

---
+++ module_intro.py
@@ -0,0 +1,24 @@
+"""模組 Module 介紹"""
+
+# import math
+import math as m
+from math import pi
+import my_module as y
+# help(math)
+
+# print(math.pi)
+print(pi)
+
+# print(m.pow(3, 2))
+
+# number = 20.678
+# c = m.ceil(number)
+# c2 = m.floor(number)
+# c3 = round(number, 2)
+
+# print(c)
+# print(c2)
+# print(c3)
+
+print(y.pi)
+print(y.cube(3))
---
+++ my_module.py
@@ -0,0 +1,14 @@
+"""計算面積"""
+pi = 3.14555
+
+def square(x):
+    """平方"""
+    return x ** 2
+
+def cube(x):
+    """立方"""
+    return x ** 3
+
+def area(radius):
+    """圓形面積"""
+    return pi * radius ** 2
