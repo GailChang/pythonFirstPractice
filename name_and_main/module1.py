e82c2cdd164baa3635c7a476a185440d88f26074
GailChang <gail863@gmail.com>
2026-01-28 21:37:24 +0800

    feat: explore __name__ and __main__

---
+++ name_and_main/module1.py
@@ -0,0 +1,9 @@
+import module2
+module2.hello()
+
+# Python 中的 if __name__ == __main__
+print(f"模組1 __name__: {__name__}")
+
+
+if __name__ == '__main__':
+    print("模組1 __name__ == __main__")
---
+++ name_and_main/module2.py
@@ -0,0 +1,11 @@
+"""模組2"""
+
+# Python 中的 if __name__ == __main__
+print(f"模組2 __name__: {__name__}")
+
+if __name__ == '__main__':
+    print("模組2 __name__ == __main__")
+
+def hello():
+    """打招呼"""
+    print("hello")
